a2457eab0d98dabd9c0cb84146d02351e5099488
Adriel P. de Guzman <adrielpdeguzman@icloud.com>
2017-04-21 15:03:07 +0800

    Create Text and Password fields components

---
+++ resources/assets/js/components/Form/Password.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const propTypes = {
+  name: PropType.string.isRequired,
+  label: PropType.string.isRequired,
+  onChange: PropType.func.isRequired,
+  placeholder: PropType.string,
+  required: PropType.bool,
+};
+
+const defaultProps = {
+  placeholder: '',
+  required: false,
+};
+
+const Password = ({ name, label, placeholder, required, onChange }) => (
+  <div className="field">
+    <label htmlFor={name} className="label">{label}</label>
+    <p className="control">
+      <input
+        type="password"
+        className="input"
+        required={required}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </p>
+  </div>
+);
+
+Password.propTypes = propTypes;
+Password.defaultProps = defaultProps;
+
+export default Password;
---
+++ resources/assets/js/components/Form/Text.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const propTypes = {
+  name: PropType.string.isRequired,
+  label: PropType.string.isRequired,
+  onChange: PropType.func.isRequired,
+  placeholder: PropType.string,
+  required: PropType.bool,
+};
+
+const defaultProps = {
+  placeholder: '',
+  required: false,
+};
+
+const Text = ({ name, label, placeholder, required, onChange }) => (
+  <div className="field">
+    <label htmlFor={name} className="label">{label}</label>
+    <p className="control">
+      <input
+        type="text"
+        className="input"
+        id={name}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </p>
+  </div>
+);
+
+Text.propTypes = propTypes;
+Text.defaultProps = defaultProps;
+
+export default Text;
